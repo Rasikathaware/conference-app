accb40476e1ed28e7e27d1e0e17d7a08ac6a1e2d
Rasikathaware <rasikathaware322@gmail.com>
2026-01-04 15:16:23 +0530

    Update speakerList.js

--- force-app/main/default/lwc/speakerList/speakerList.js
+++ force-app/main/default/lwc/speakerList/speakerList.js
@@ -1,3 +1,38 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import SPEAKER_SELECTED from '@salesforce/messageChannel/SpeakerSelected__c';
 
-export default class SpeakerList extends LightningElement {}
+export default class SpeakerList extends LightningElement {
+
+    @api speakers;
+
+    @wire(MessageContext)
+    messageContext;
+    columns =[
+        { label:'Name', fieldName:'Name'},
+        { label:'Email', fieldName:'Email__c'},
+        { label:'Speciality', fieldName:'Speciality__c'}, 
+        {
+            type:'button',
+            typeAttributes:{
+                label:'Book Session',
+                name:'book',
+                variant:'brand'
+            }
+        }   
+
+    ];
+
+    handleRowAction(event)
+    {
+        
+        const speakerId = event.detail.row.Id;
+
+
+        console.log('Selected Speaker:', speakerId);
+
+        publish(this.messageContext, SPEAKER_SELECTED, {
+            speakerId: speakerId
+        });
+    }
+}
