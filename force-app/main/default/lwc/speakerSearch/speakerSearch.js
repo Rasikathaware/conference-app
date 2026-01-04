63262a56a387ab3bb653deeeaef447577487e0e6
Rasikathaware <rasikathaware322@gmail.com>
2026-01-04 15:17:18 +0530

    Update speakerSearch.js

--- force-app/main/default/lwc/speakerSearch/speakerSearch.js
+++ force-app/main/default/lwc/speakerSearch/speakerSearch.js
@@ -1,3 +1,43 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import searchSpeakers from '@salesforce/apex/SpeakerController.searchSpeakers';
 
-export default class SpeakerSearch extends LightningElement {}
+export default class SpeakerSearch extends LightningElement {
+
+    name;
+    speciality;
+
+    @track speakers = [];
+
+    get hasResults() {
+        return this.speakers && this.speakers.length > 0;
+    }
+
+    specialityOptions = [
+        { label: 'Apex', value: 'Apex' },
+        { label: 'LWC', value: 'LWC' },
+        { label: 'Integrations', value: 'Integrations' },
+        { label: 'Architecture', value: 'Architecture' }
+    ];
+
+    handleName(event) {
+        this.name = event.target.value;
+    }
+
+    handleSpeciality(event) {
+        this.speciality = event.target.value;
+    }
+
+    handleSearch() {
+        searchSpeakers({
+            name: this.name,
+            speciality: this.speciality
+        })
+        .then(result => {
+            this.speakers = result;
+            console.log('Speakers:', result);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }
+}
